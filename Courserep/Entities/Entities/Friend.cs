dc3767d325101acc244df2ddda9fcf935d38fb42
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-06-09 05:08:26 +0300

    friendUpadtes

--- Courserep/Entities/Entities/Friend.cs
+++ Courserep/Entities/Entities/Friend.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,11 @@ namespace Entities.Entities
 {
     public class Friend
     {
+        [Column("FriendId")]
         public int Id { get; set; } 
+
+        [ForeignKey(nameof(User))]
+        public Guid UserId { get; set; }
+        public User User { get; set; }
     }
 }
