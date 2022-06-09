09e2899f89badb7b9fe57422c3a47ef6e80940a5
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-06-09 04:48:39 +0300

    UserUpdated

--- Courserep/Entities/Entities/User.cs
+++ Courserep/Entities/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,21 @@ namespace Entities.Entities
 {
     public class User
     {
+        [Column("UserId")]
         public   Guid Id { get; set; }
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public DateTime Created { get; set; }
+
+        public ICollection<Message> Messages { get; set; }
+        public ICollection<Camp> Camps { get; set; }
+        public ICollection<CampClass> CampClasses { get; set; }
+        public ICollection<Friend> Friends { get; set; }
+
+
+
 
     }
 }
