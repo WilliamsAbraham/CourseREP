4853143d7aeda039f1a55592b668288c6f3bc338
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-06-10 08:33:54 +0300

    CorsConguration

--- Courserep/Courserep/ExtensionService/ExtensionService.cs
+++ Courserep/Courserep/ExtensionService/ExtensionService.cs
@@ -1,7 +1,18 @@
-﻿namespace Courserep.ExtensionService
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Courserep.ExtensionService
 {
     public static class ExtensionService
     {
+        public static void ConfigureCors(this IServiceCollection service)
+        {
+            service.AddCors(opt =>
+           opt.AddPolicy("CorsPolicy", policy =>
+           policy.AllowAnyHeader()
+           .AllowAnyMethod()
+           .AllowAnyOrigin()
+           .AllowAnyMethod()));
+        }
 
 
 
