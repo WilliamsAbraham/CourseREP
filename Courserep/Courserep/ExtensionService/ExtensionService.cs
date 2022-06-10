914afd0298da68b366c498ff9513b95978b60520
Abraham <68301663+WilliamsAbraham@users.noreply.github.com>
2022-06-10 08:46:22 +0300

    IISOptions confurations

--- Courserep/Courserep/ExtensionService/ExtensionService.cs
+++ Courserep/Courserep/ExtensionService/ExtensionService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Courserep.ExtensionService
 {
@@ -14,7 +15,8 @@ namespace Courserep.ExtensionService
            .AllowAnyMethod()));
         }
 
-
+        public static void ConfigureIISOptions(this IServiceCollection service) =>
+            service.Configure<IISOptions>(opt => { });
 
 
     }
--- Courserep/Courserep/Startup.cs
+++ Courserep/Courserep/Startup.cs
@@ -27,6 +27,7 @@ namespace Courserep
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureIISOptions();
             services.ConfigureCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
